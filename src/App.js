f270b72cfa69cfa519b924ffaef46742de9a533b
Juan Carlos López <juancarlos9228@outlook.es>
2023-08-15 18:26:27 -0700

    Agregué descripciones de los servicios en los botones del body

--- src/App.js
+++ src/App.js
@@ -7,21 +7,15 @@ function App() {
   const [selectedCategory, setSelectedCategory] = useState('');
   const [showSubMenu, setShowSubMenu] = useState(false);
   const [showSubMenuOnSlideChange, setShowSubMenuOnSlideChange] = useState(false);
-  const [showPostCarousel, setShowPostCarousel] = useState(false);
-  const [selectedPost, setSelectedPost] = useState(null);
 
   const handleCategoryClick = (category) => {
     if (category === selectedCategory) {
       setShowSubMenu(!showSubMenu);
       setShowSubMenuOnSlideChange(!showSubMenuOnSlideChange);
-      setShowPostCarousel(false);
-      setSelectedPost(null);
     } else {
       setSelectedCategory(category);
       setShowSubMenu(true);
       setShowSubMenuOnSlideChange(true);
-      setShowPostCarousel(false);
-      setSelectedPost(null);
     }
   };
 
@@ -31,39 +25,48 @@ function App() {
     }
   };
 
-  const handleSubMenuButtonClick = () => {
-    setShowSubMenu(!showSubMenu);
-    setShowSubMenuOnSlideChange(!showSubMenuOnSlideChange);
-    setShowPostCarousel(false);
-    setSelectedPost(null);
+  const categoryButtons = ['Centro de Atención Múltiple (CAM)', 'Educación Inicial', 'Preescolar', 'Primaria', 'Secundaria'];
+
+  const categoryInfo = {
+    'Centro de Atención Múltiple (CAM)': (
+      <p>
+        Los Centros de Atención Múltiple (CAM) son servicios escolarizados que atienden a alumnas y alumnos con discapacidad, discapacidad múltiple y Trastorno del Espectro Autista (TEA) en situaciones excepcionales, los cuales requieren de ajustes razonables significativos y en ocasiones de apoyos no solo más especializados sino permanentes. Estos centros ofrecen formación para la vida y a lo largo de la misma, así como el desarrollo de su autonomía e independencia, a través del tránsito de los niveles de educación inicial, preescolar, primaria y secundaria.
+      </p>
+    ),
+    'Educación Inicial': (
+      <p>
+        Tiene como propósito favorecer el desarrollo motor, físico, cognitivo, afectivo y social de los menores de cuatro años de edad. Incluye orientación a padres de familia o tutores para la educación de sus hijas, hijos o pupilos.
+      </p>
+    ),
+    Preescolar: (
+      <p>
+        Es el primer nivel de la educación básica. Busca el desarrollo integral y equilibrado que facilita la relación de los niños con sus pares y con adultos. Promueve la socialización y la afectividad, las capacidades de comunicación, el pensamiento matemático, el conocimiento de los entornos natural y social, el desarrollo y enriquecimiento físico y psicomotriz, y la expresión artística.
+      </p>
+    ),
+    Primaria: (
+      <p>
+         Es el segundo nivel del tipo de educación básica. Su 
+objetivo es sentar las bases en áreas 
+fundamentales para la adquisición 
+de conocimientos posteriores, esto 
+es, el dominio de la lectoescritura, el 
+conocimiento matemático aritmético 
+y geométrico, el conocimiento básico 
+de las ciencias naturales y sociales, el 
+conocimiento del cuerpo, las posibilidades motrices y las emociones, las 
+artes y las relaciones interpersonales.
+      </p>
+    ),
+    Secundaria: (
+      <p>
+         Es el tercer nivel del tipo de educación básica. Su 
+objetivo es ofrecer conocimientos y habilidades avanzados, que consoliden 
+los procesos iniciados en los niveles 
+anteriores, y que sienten bases para 
+las áreas especializadas que se estudian en la educación media superior
+      </p>
+    ),
   };
-
-  const handleBlogButtonClick = () => {
-    setShowSubMenu(!showSubMenu);
-    setShowSubMenuOnSlideChange(!showSubMenuOnSlideChange);
-    setShowPostCarousel(true);
-    setSelectedPost(null);
-  };
-
-  const handlePostClick = (post) => {
-    setSelectedPost(post);
-  };
-
-  const categoryButtons = ['Educación Inicial', 'Preescolar', 'Primaria', 'Secundaria', 'Talleres'];
-
-  const posts = [
-    {
-      id: 1,
-      title: 'Título del Post 1',
-      content: 'Contenido completo del Post 1',
-    },
-    {
-      id: 2,
-      title: 'Título del Post 2',
-      content: 'Contenido completo del Post 2',
-    },
-    // Agrega más posts aquí
-  ];
 
   return (
     <div className="App">
@@ -91,26 +94,8 @@ function App() {
       </div>
       {showSubMenu && selectedCategory !== 'Inicio' && selectedCategory !== 'Registrar' && selectedCategory !== 'Entrar' && selectedCategory !== 'Contacto' && (
         <div className="sub-menu">
-          <button onClick={handleBlogButtonClick}>Blog</button>
-          <button>Videos</button>
-        </div>
-      )}
-      {showPostCarousel && selectedPost === null && (
-        <div className="post-carousel">
-          {/* Carrusel de posts */}
-          {posts.map((post) => (
-            <div key={post.id} className="post-slide" onClick={() => handlePostClick(post)}>
-              <h3>{post.title}</h3>
-              <p>{post.content}</p>
-            </div>
-          ))}
-        </div>
-      )}
-      {selectedPost && (
-        <div className="full-post" onClick={() => setSelectedPost(null)}>
-          {/* Muestra el texto completo del post */}
-          <h3>{selectedPost.title}</h3>
-          <p>{selectedPost.content}</p>
+          {/* Mostrar cuadro de texto con información */}
+          {categoryInfo[selectedCategory]}
         </div>
       )}
     </div>
@@ -120,6 +105,8 @@ function App() {
 export default App;
 
 // Resto del código CSS ...
+
+
 
 
 
